84f0d20c1506abf460216466d1f2015cbde41a5a
TiagoAryan <tiago_aryan@hotmail.com>
2021-04-14 20:06:07 +0100

    commit

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+  function slide(e) {
+    const sliderPos = e.target.value;
+
+    // Update the width of the foreground image
+    document.getElementsByClassName("foreground-img")[0].style.width =
+      sliderPos + "%";
+    // Update the position of the slider button
+    document.getElementsByClassName("slider-button")[0].style.left =
+      "calc(" + sliderPos + "% - 18px)";
+  }
+</script>
+
+<div class="container">
+  <div class="img background-img" />
+  <div class="img foreground-img" />
+  <input
+    type="range"
+    min="1"
+    max="100"
+    value="50"
+    class="slider"
+    name="slider"
+    id="slider"
+    on:input={slide}
+  />
+  <div class="slider-button" />
+</div>
